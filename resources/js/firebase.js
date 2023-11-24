ffdf681290a7524797386d322ef5a433958f48eb
joohyung <06hong09@naver.com>
2023-11-24 16:35:52 +0900

    init

---
+++ resources/js/firebase.js
@@ -0,0 +1,75 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.6.0/firebase-app.js";
+import {
+  getFirestore,
+  collection,
+  getDocs,
+  setDoc,
+  addDoc,
+  doc,
+  deleteDoc,
+  updateDoc,
+  getDoc,
+} from "https://www.gstatic.com/firebasejs/10.6.0/firebase-firestore.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.6.0/firebase-analytics.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCaqy1g6UNfgLUzUxxSlbdKO2gUZ07qjdI",
+  authDomain: "test-project-347ef.firebaseapp.com",
+  projectId: "test-project-347ef",
+  storageBucket: "test-project-347ef.appspot.com",
+  messagingSenderId: "531855850029",
+  appId: "1:531855850029:web:ad9c1082768469f8e4d6a4",
+  measurementId: "G-M79MSJ8QL2",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const db = getFirestore(app);
+
+async function getDatas(collectionName) {
+  const querySnapshot = await getDocs(collection(db, collectionName));
+  return querySnapshot;
+}
+
+async function addDatas(collectionName, dataObj) {
+  // 문서ID 부여
+  //   await setDoc(doc(db, "member", "member1"), dataObj);
+
+  // 문서ID 자동
+  await addDoc(collection(db, collectionName), dataObj);
+}
+
+async function deleteDatas(collectionName, docId) {
+  await deleteDoc(doc(db, collectionName, docId));
+}
+
+async function updateDatas(collectionName, docId, updateInfoObj) {
+  const docRef = await doc(db, collectionName, docId);
+  const docData = await getDoc(docRef);
+  console.log(docData.data());
+  // debugger;
+  // 문서 필드 데이터 수정
+  await updateDoc(docRef, updateInfoObj);
+}
+
+export {
+  db,
+  getDocs,
+  collection,
+  getDatas,
+  setDoc,
+  addDoc,
+  doc,
+  addDatas,
+  deleteDoc,
+  deleteDatas,
+  updateDoc,
+  updateDatas,
+};
