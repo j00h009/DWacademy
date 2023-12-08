95b6fcda8b8967a98dcc62260dd17e3f2249b93c
JooHyung <06hong09@naver.com>
2023-12-08 13:49:25 +0900

    react +

--- react/01-dice_game/src/Board.js
+++ react/01-dice_game/src/Board.js
@@ -1,19 +1,16 @@
 import Dice from "./Dice";
+import "./Board.css";
 
-// ceil : 소수점 올림
-function random(n) {
-  return Math.ceil(Math.random() * n);
-}
-
-function Board({ name, color }) {
+function Board({ name, color, num, gameHistory }) {
+  // const { name, color } = props;
   return (
-    <div>
-      <h2>{name}</h2>
-      <Dice color={color} num={random(6)} />
-      <h2>총점</h2>
+    <div className="Board App-board">
+      <h2 className="Board-heading">{name}</h2>
+      <Dice color={color} num={num} />
+      <h2 className="Board-heading">총점</h2>
       <p>0</p>
-      <h2>기록</h2>
-      <p>0</p>
+      <h2 className="Board-heading">기록</h2>
+      <p>{gameHistory.join(", ")}</p>
     </div>
   );
 }
--- react/01-dice_game/src/index.js
+++ react/01-dice_game/src/index.js
@@ -1,6 +1,8 @@
-import React from "react";
+import React, { Fragment } from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.js";
+import "./index.css";
+import State from "./State.js";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
@@ -8,6 +10,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 // const price = 2000;
 
 root.render(<App />);
+// root.render(<State />);
 
 // root.render(
 //   <div>
